103ae4faca2e13f65823651a2e94ef056d22dfd2
rafaelpsbpinhal@gmail.com <rafaelpsbpinhal@gmail.com>
2026-06-11 12:11:10 -0300

    Limpeza automática dos campos

---
+++ script.js
@@ -0,0 +1,65 @@
+function executarSistema() {
+    // dados de entrada 
+    const nome = document.getElementById("inputNome").value;
+    const idade = parseInt(document.getElementById("inputIdade").value);
+    const valor = parseFloat(document.getElementById("inputValor").value);
+    const cupom = document.getElementById("inputCupom").value === "true"
+
+    // dados de saída 
+    const msg = document.getElementById("mensagem-autorizacao");
+    const lista = document.getElementById("lista-estoque");
+    const relatorio = document.getElementById("relatorio-final");
+
+    // validação para cmapos vazios 
+    if (!nome || isNaN(idade) || isNaN(valor)) {
+        alert("Por favor, peencha todos os campos!!");
+        return;
+    }
+
+    //regra de negócios 
+    if (idade >= 16) {
+        msg.innerText = `Venda autorizada: ${nome}`;
+        msg.style.color = "#00ff88";
+
+        //desconto
+        let valorFinal = (valor > 500 || cupom) ? valor * 0.85 : valor;
+
+        // estoque
+        let estoque = ["placa de video", "processador", "memoria RAM"];
+        lista.innerHTML = ""; // limpa a lista anterior
+
+        // forEach:percorre um array e aplica uma ação para cada elemento
+        estoque.forEach(item => {
+            let li = document.createElement("li");
+            li.innerText = `Item ${item} reservado.`;
+            lista.appendChild(li); // usado para adicionar um novo elemento ou texto
+        });
+
+        //reatorio
+        relatorio.style.display = "block";
+        relatorio.innerHTML = `
+        <strong> RESUMO DO PEDIDO <\strong><br>
+        cliente: ${nome}<br>
+        Total Original: R$ ${valor.toFixed(2)} <br>
+        <strong> Total com descoto: R$ ${valorFinal.toFixed(2)} <\strong>
+        `;
+
+        // ==========================================================
+        // TRECHO ADICIONADO PARA A SUA QUESTÃO (LIMPEZA DOS CAMPOS):
+        // ==========================================================
+        document.getElementById("inputNome").value = "";
+        document.getElementById("inputIdade").value = "";
+        document.getElementById("inputValor").value = "";
+        document.getElementById("inputCupom").value = "false"; 
+        // ==========================================================
+
+    } else {
+        msg.innerText = "Venda bloqueada: menor de 16 anos.";
+        msg.style.color = "#f0f4444";
+        relatorio.style.display = "none";
+        lista.innerHTML = "";
+
+    }
+
+
+}
